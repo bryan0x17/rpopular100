0330bcebfad3457047166c6f529198ff23d1f2bb
Bryan <50989230+bryan0x17@users.noreply.github.com>
2022-06-14 15:20:55 -0600

    Add API call

--- reddit-client/src/app/store.js
+++ reddit-client/src/app/store.js
@@ -5,3 +5,14 @@ export const store = configureStore({
     
   },
 });
+
+export const getData = async (after) => {
+  const options = {
+    method: 'GET',
+    body: JSON.stringify(after)
+  };
+  const url = 'https://www.reddit.com/r/popular.json';
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+}
