8fc75c4c2845054fe45a52ccd2fbc3f99e780799
Bryan <50989230+bryan0x17@users.noreply.github.com>
2022-06-14 16:56:40 -0600

    Add async thunk to postSlice

--- reddit-client/src/features/post/postSlice.js
+++ reddit-client/src/features/post/postSlice.js
@@ -1,4 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const loadPosts = createAsyncThunk(
+    'post/loadPosts',
+    async (after, thunkAPI) => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(after)
+        };
+        const url = 'https://www.reddit.com/r/popular.json';
+        const response = await fetch(url, options);
+        const data = await response.json();
+        return data;
+    }
+);
 
 export const postSlice = createSlice({
     name: 'post',
@@ -12,8 +29,15 @@ export const postSlice = createSlice({
             state.after = after;
             state.posts = state.posts.concat(posts);
         }
+    },
+    extraReducers: {
+        [loadPosts.fulfilled]: (state, action) => {
+            state.after = action.payload.data.after;
+            state.posts.concat(action.payload.data.children); 
+        }
     }
 });
+
 
 
 export const { addPosts } = postSlice.actions;
