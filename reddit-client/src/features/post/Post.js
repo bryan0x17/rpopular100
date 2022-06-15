60db405da788ae4eb994aba5576f4c3332e550a1
Bryan <50989230+bryan0x17@users.noreply.github.com>
2022-06-15 15:07:50 -0600

    Update Post.js

--- reddit-client/src/features/post/Post.js
+++ reddit-client/src/features/post/Post.js
@@ -4,14 +4,14 @@ export const Post = (props) => {
     const post = props.post.data;
     const date = new Date(post.created_utc * 1000);
     const postUrl = `https://www.reddit.com${post.permalink}`;
-    const subredditUrl = `https://www.reddit.com${post.subreddit_name_prefixed}`
+    const subredditUrl = `https://www.reddit.com/${post.subreddit_name_prefixed}`
 
     return (
         <div className="post">
             <h2 className="post-title"><a href={postUrl} target="_blank" rel="noreferrer">{post.title}</a></h2>
             <h3 className="post-subreddit">posted in <a href={subredditUrl} target="_blank" rel="noreferrer">{post.subreddit_name_prefixed}</a></h3>
             {post.is_video &&
-                <video controls>
+                <video controls width={720}>
                     <source src={post.media.reddit_video.fallback_url}
                         type="video/webm" />
                     Sorry, your browser doesn't support embedded videos.
