e140ad9bc90e689e5f2259e568fbc439ebc04ab5
Bryan <50989230+bryan0x17@users.noreply.github.com>
2022-06-15 12:53:00 -0600

    Add dispatch call to App

--- reddit-client/src/App.js
+++ reddit-client/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { NavBar } from './features/navbar/NavBar';
 import { Post } from './features/post/Post';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadPosts, selectPosts } from './features/post/postSlice';
 
 function App() {
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadPosts())
+  }, [dispatch]);
+  const posts = useSelector(selectPosts);
+
   return (
     <div className="App">
-      <NavBar/>
-      <Post/>
+      <NavBar />
+      {posts.map((post, index) => {
+        return <Post
+          post={post}
+          key={index}
+        />
+      })}
     </div>
   );
 }
